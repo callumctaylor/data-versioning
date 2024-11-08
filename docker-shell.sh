727d4f36ed332fbae1b15e95f9add5eee16230a6
CallumTaylor3130 <callumtaylor3130@gmail.com>
2024-11-07 20:47:26 -0500

    dataset updates...

--- docker-shell.sh
+++ docker-shell.sh
@@ -4,10 +4,10 @@ set -e
 
 export BASE_DIR=$(pwd)
 export SECRETS_DIR=$(pwd)/../secrets/
-export GCS_BUCKET_NAME="cheese-app-data-versioning"
-export GCP_PROJECT="ac215-project"
-export GCP_ZONE="us-central1-a"
-export GOOGLE_APPLICATION_CREDENTIALS="/secrets/data-service-account.json"
+export GCS_BUCKET_NAME="cheese-app-data-versioning-callum"
+export GCP_PROJECT="AC215"
+export GCP_ZONE="us"
+export GOOGLE_APPLICATION_CREDENTIALS="../secrets/data-service-account.json"
 
 
 echo "Building image"
